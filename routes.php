58a1da8d3d65606afc619bc453b51a791443a816
FrederikBA <frederikbandersen98@gmail.com>
2023-08-30 12:10:36 +0200

    add frontend

--- routes.php
+++ routes.php
@@ -4,5 +4,5 @@ require_once __DIR__ . '/router.php';
 
 get('/security', '/public/index.php');
 post('/security/product/create', 'src/Endpoints/CreateProduct.php');
-post('/security/test', 'src/Endpoints/TestEndpoint.php');    
+get('/security/test', 'src/Endpoints/TestEndpoint.php');    
 #any('/404', 'views/404.php');
