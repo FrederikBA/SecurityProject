a55d56f89057d10a8fbb5b53f687099e80a1b924
Janussr <Janusstivang@gmail.com>
2023-08-31 18:12:54 +0200

    add new SQL scripts

---
+++ queries/drop_tables_new.sql
@@ -0,0 +1,9 @@
+USE shopdb;
+
+
+-- Drop tables in reverse order to handle dependencies
+DROP TABLE IF EXISTS OrderLine;
+DROP TABLE IF EXISTS `Order`;
+DROP TABLE IF EXISTS UserRole;
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS User;
