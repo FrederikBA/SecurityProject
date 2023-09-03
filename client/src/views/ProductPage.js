cfe31a3e3e2d611f7098510f5fef8564bd8fd821
FrederikBA <frederikbandersen98@gmail.com>
2023-09-03 19:24:07 +0200

    Frontend - render 404 on nonexistant products

--- client/src/views/ProductPage.js
+++ client/src/views/ProductPage.js
@@ -6,27 +6,39 @@ import shirt from '../img/shirt.jpg';
 
 const ProductPage = () => {
     const [product, setProduct] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
     const { productId } = useParams();
 
     const URL = apiUtils.getUrl();
 
     useEffect(() => {
         const getProductById = async () => {
-            const response = await apiUtils.getAxios().get(URL + '/product/' + productId)
-            setProduct(response.data)
+            try {
+                const response = await apiUtils.getAxios().get(URL + '/product/' + productId)
+                setProduct(response.data)
+                setIsLoading(false)
+            } catch (error) {
+            }
         }
         getProductById()
     }, []);
 
     return (
         <div className="center">
-            <div className="product-container">
-                <div className="product-card">
-                    <img src={shirt} alt={product.product_name} className="product-image" />
-                    <h2 className="product-name">{product.product_name}</h2>
-                    <p className="product-price">$ {product.product_price}</p>
+            {isLoading ? (
+                <div>
+                    <h1>404</h1>
+                    <h4>Product doesn't exist</h4>
                 </div>
-            </div>
+            ) : (
+                <div className="product-container">
+                    <div className="product-card">
+                        <img src={shirt} alt={product.product_name} className="product-image" />
+                        <h2 className="product-name">{product.product_name}</h2>
+                        <p className="product-price">$ {product.product_price}</p>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
