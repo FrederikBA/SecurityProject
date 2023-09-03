faff56737e4030a08c35a93ede17fbd6b4a601cd
Janussr <Janusstivang@gmail.com>
2023-09-03 13:09:57 +0200

    Change error msg in product

--- queries/drop_tables.sql
+++
@@ -1,9 +0,0 @@
--- Drop tables with foreign key relationships first
-DROP TABLE OrderLine;
-DROP TABLE `Order`;
-DROP TABLE UserLogin;
-DROP TABLE UserRole;
-
--- Drop other tables
-DROP TABLE Product;
-DROP TABLE User;
