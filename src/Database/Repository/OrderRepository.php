669ac4bb7e319e930cd49e9a2555f6b54327030f
FrederikBA <frederikbandersen98@gmail.com>
2023-09-06 00:22:54 +0200

    WIP: OrderRepository

---
+++ src/Database/Repository/OrderRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+class OrderRepository extends Connector
+{
+    public function GetOrder(int $id)
+    {
+        //TODO Implement
+    }
+
+    public function GetAllOrders()
+    {
+        //TODO Implement
+    }
+
+    public function CreateOrder()
+    {
+        //TODO Implement
+    }
+
+    public function UpdateOrder()
+    {
+        //TODO Implement
+    }
+}
