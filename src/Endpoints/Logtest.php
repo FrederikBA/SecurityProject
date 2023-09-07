a5eb7f4f280b91b6ca39e48fb5e6a8d82597893c
Janussr <Janusstivang@gmail.com>
2023-09-07 16:39:45 +0200

    log test works

--- src/Endpoints/Logtest.php
+++ src/Endpoints/Logtest.php
@@ -1,20 +1,24 @@
-<?php
-require 'src/Logging/Logger.php'; // Include the logger configuration file
 
+<?php
+require 'src/Logging/LoggingManager.php'; // Include the LoggingManager class
+
+// Create a 'security' logger instance
+$securityLoggerManager = new LoggingManager('security', 'Logs/security.log');
+$securityLogger = $securityLoggerManager->getLogger();
 
 // Log some messages at different log levels
-$logger->info('This is an informational message.');
-$logger->warning('This is a warning message.');
-$logger->error('This is an error message.');
-$logger->debug('This is a debug message.');
+$securityLogger->info('This is an informational message.');
+$securityLogger->warning('This is a warning message.');
+$securityLogger->error('This is an error message.');
+$securityLogger->debug('This is a debug message.');
 
 try {
     // Simulate an exception
     throw new Exception('This is a simulated exception.');
 } catch (Exception $e) {
     // Log the exception and its stack trace
-    $logger->error('Caught exception: ' . $e->getMessage());
-    $logger->error('Exception stack trace: ' . $e->getTraceAsString());
+    $securityLogger->error('Caught exception: ' . $e->getMessage());
+    $securityLogger->error('Exception stack trace: ' . $e->getTraceAsString());
 }
 
 echo 'Logging test completed.';
--- src/Logging/LoggingManager.php
+++ src/Logging/LoggingManager.php
@@ -11,14 +11,19 @@ class LoggingManager
 
     public function __construct($logName, $logFile)
     {
-        // Create a logger instance
-        $this->logger = new Logger($logName);
+        try {
+            // Create a logger instance
+            $this->logger = new Logger($logName);
 
-        // Create a log file handler (you can change the log file path)
-        $handler = new StreamHandler($logFile, Logger::DEBUG);
+            // Create a log file handler (you can change the log file path)
+            $handler = new StreamHandler($logFile, Logger::DEBUG);
 
-        // Add the handler to the logger
-        $this->logger->pushHandler($handler);
+            // Add the handler to the logger
+            $this->logger->pushHandler($handler);
+        } catch (Exception $e) {
+            // Handle any exceptions during logger initialization
+            echo 'Error initializing logger: ' . $e->getMessage();
+        }
     }
 
     public function getLogger()
